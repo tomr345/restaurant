0dd09a093cc91c82ccc2e7cf455cfe8bcbcf4e86
Tom Rintoul <thomas.rintoul@icloud.com>
2022-05-31 22:21:24 +0100

    first two tabs working

--- src/index.js
+++ src/index.js
@@ -2,4 +2,29 @@ import generateHomepage from './home';
 import generateMenu from './menu';
 
 generateHomepage();
-// generateMenu();
+initialiseListeners();
+
+function refresh() {
+    const content = document.getElementById('content');
+    content.replaceChildren();
+}
+
+function homepage() {
+    refresh();
+    generateHomepage();
+    initialiseListeners();
+
+}
+
+function menu() {
+    refresh();
+    generateMenu();
+    initialiseListeners();
+}
+
+function initialiseListeners() {
+    const homeBtn = document.querySelector('.home');
+    homeBtn.addEventListener('click', homepage);
+    const menuBtn = document.querySelector('.menu');
+    menuBtn.addEventListener('click', menu);
+}
